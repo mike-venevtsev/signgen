8ab6cc9a161c6333d6ad1983730e7e1ea1cb8249
Slava Tsyrulnik <neemah86@gmail.com>
2013-03-20 18:24:10 +0400

    * README and PHPDoc

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
 Signgen
-===================
+=======
 
 Signature generator for various devcookies software API
 
 PHP
 ---
 
-TODO
+Supported version: 5.3+
+
+Code example:
+
+	<?php
+	require_once "path/to/php/devcookies/SignatureGenerator.php";
+	
+	$generator = new SignatureGenerator("my_salt_from_manager");
+
+	$requestData = array(
+		"site_id" => 100239,
+		"action" => "createorder",
+		"...",
+	);
+	
+	$requestParams["signature"] = $generator->assemble($requestData);
+	?>
 
 License
 -------
 
-TODO
+MIT License
--- php/devcookies/SignatureGenerator.php
+++ php/devcookies/SignatureGenerator.php
@@ -4,6 +4,26 @@ namespace devcookies;
 
 use \InvalidArgumentException;
 
+/**
+ * 
+ * @author devcookies
+ * @copyright 2013 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 class SignatureGenerator 
 {
 	private $salt = "";
@@ -28,7 +48,7 @@ class SignatureGenerator
 	}
 	
 	/**
-	 * @param array $params
+	 * @param array $params supported types for array values (of any level): boolean, string, array or null
 	 * @throws InvalidArgumentException
 	 * @return string
 	 */
@@ -67,7 +87,10 @@ class SignatureGenerator
 					
 				default:
 					if (!$this->ignoreInvalidValues)
-						throw new InvalidArgumentException("Type of value for key: \"{$key}\" is not supported. Supported types are: boolean, string, array and null");
+						throw new InvalidArgumentException(
+							"Type of value for key: \"{$key}\" is not supported."
+							. " Supported types are: boolean, string, array and null"
+						);
 					continue 2;
 			}
 			
